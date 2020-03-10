0b698708d5ede9964113fc5d538ab4e3226dafa7
nicksmith105 <30782766+nicksmith105@users.noreply.github.com>
2020-03-10 12:31:25 -0700

    updates

---
+++ .cache/caches/gatsby-plugin-typography/typography.js
@@ -0,0 +1,1 @@
+module.exports = require("/Users/Nick/Desktop/Port3/src/utils/typography")
---
+++ .cache/sync-requires.js
@@ -0,0 +1,13 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/Nick/Desktop/Port3/.cache/dev-404-page.js"))),
+  "component---src-pages-about-js": hot(preferDefault(require("/Users/Nick/Desktop/Port3/src/pages/about.js"))),
+  "component---src-pages-contact-js": hot(preferDefault(require("/Users/Nick/Desktop/Port3/src/pages/contact.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/Nick/Desktop/Port3/src/pages/index.js")))
+}
+
