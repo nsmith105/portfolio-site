bfff4270ac0971c59d4d1fae06c45e2924941926
nicksmith105 <30782766+nicksmith105@users.noreply.github.com>
2020-03-09 17:31:32 -0700

    basic site bones, ready to go

---
+++ src/pages/contact.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default () => (
+    <Layout>
+        <h1>I'd love to talk! Here's my email</h1>
+        <p>
+            <a href="mailto:njsmith105@gmail.com">my email</a>
+        </p>
+    </Layout>
+)
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default () => (
+    <Layout>
+        <h1>Hi! Im building a site</h1>
+        <p>
+            What do I like to do? Of course build websites
+        </p>
+    </Layout>
+)
